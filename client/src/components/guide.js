23e65375c61c70f7b07899fc14a07bbc7ce3014d
AndrewH-6400 <drewh6400@gmail.com>
2025-01-29 16:44:48 -0500

    Guide component is making a proof of concept call to the local server to get information from the spotify api
    Signed-off-by: AndrewH-6400 <drewh6400@gmail.com>

--- client/src/components/guide.js
+++ client/src/components/guide.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Guide() {
+    /*
+    returns an array of the album search results to be sorted
+    url is to the local server which will handle the call to spotify api and format the data (mostly)
+    information I want to be handled here is :Limit(number of results), offset(information for displaying multiple pages very important for search)
+    ,search parameter
+    eventually possibly the type as well (album vs song/nothing)
+    */
+    useEffect(() => {
+        fetch("http://localhost:8080/search/al")
+            .then((response) => response.json())
+            .then((data) => console.log(data.albums.items));
+    }, []);
+
     return <div>Guide</div>;
 }
 
